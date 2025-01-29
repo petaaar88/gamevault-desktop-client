b0b9454d2274d950633fa7b2e1781655eaee2f85
petaaar88 <pe.bn80@gmail.com>
2025-01-29 18:48:00 +0100

    Add search component

--- src/main/java/org/example/desktopclient/component/SearchComponent.java
+++ src/main/java/org/example/desktopclient/component/SearchComponent.java
@@ -1,15 +1,61 @@
 package org.example.desktopclient.component;
 
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class SearchComponent {
-    public HBox getComponent() {
-        HBox layout = new HBox();
-        TextField textField = new TextField("Unestite nesto");
 
-        layout.getChildren().add(textField);
+    TextField textField;
+
+    public HBox getComponent(String placeholder) {
+        HBox layout = new HBox();
+
+        String css = getClass().getResource("/org/example/desktopclient/styles/searchComponent.css").toExternalForm();
+        layout.getStylesheets().add(css);
+
+        textField = new TextField();
+        textField.getStyleClass().add("search-text");
+
+        ImageView imageView = new ImageView(new Image(getClass().getResource("/org/example/desktopclient/icons/searchIcon1.png").toExternalForm()));
+
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+
+        Button button = new Button("", imageView);
+        button.getStyleClass().add("search-button");
+        StackPane stackPane = new StackPane(button);
+        stackPane.setMaxHeight(33); // Veličina dugmeta
+        stackPane.setTranslateX(-42); // Pomeranje dugmeta unutar textField-a
+        StackPane.setAlignment(button, Pos.CENTER_RIGHT);
+
+
+        textField.requestFocus();
+
+        textField.setPromptText(placeholder);
+        layout.setMaxWidth(1000);
+        layout.setMinWidth(1000);
+        layout.getChildren().addAll(textField, stackPane);
+
         return layout;
     }
+
+    public void settingFocus(Scene scene) {
+
+        Platform.runLater(() -> textField.getParent().requestFocus());
+        scene.setOnMousePressed(event -> {
+            if (!textField.isFocused()) {
+                return;
+            }
+            textField.getParent().requestFocus(); // Fokusira roditelj, tj. defokusira TextField
+        });
+    }
+
 }
 
--- src/main/java/org/example/desktopclient/scene/GameCatalogScene.java
+++ src/main/java/org/example/desktopclient/scene/GameCatalogScene.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.example.desktopclient.component.MenuComponent;
@@ -14,7 +15,11 @@ import org.example.desktopclient.component.SearchComponent;
 public class GameCatalogScene {
     public Scene createScene(Stage primaryStage) {
 
-        StackPane layout = new StackPane();
+        VBox layout = new VBox();
+        layout.setPrefWidth(Double.MAX_VALUE);  // Postavlja širinu na maksimalnu
+        layout.setPrefHeight(Double.MAX_VALUE);  // Postavlja visinu na maksimalnu
+
+        layout.setAlignment(Pos.TOP_CENTER);
 
         Scene scene = new Scene(layout, 1200, 768);
         String css = getClass().getResource("/org/example/desktopclient/styles/universalStyles.css").toExternalForm();
@@ -24,8 +29,10 @@ public class GameCatalogScene {
 
         MenuComponent menuComponent = new MenuComponent();
         SearchComponent searchComponent = new SearchComponent();
+        searchComponent.settingFocus(scene);
 
-        layout.getChildren().addAll(menuComponent.getComponent(), searchComponent.getComponent());
+
+        layout.getChildren().addAll(menuComponent.getComponent(), searchComponent.getComponent("Search Games"));
         layout.setPadding(new Insets(25, 0, 0, 0)); // (top, right, bottom, left)
 
         primaryStage.setMinWidth(1048);
