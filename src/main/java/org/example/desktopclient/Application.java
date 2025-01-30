85bfe62e982c4d40441b6e988b6354beb1371edf
petaaar88 <pe.bn80@gmail.com>
2025-01-30 12:20:50 +0100

    refactor search bar style

--- src/main/java/org/example/desktopclient/Application.java
+++ src/main/java/org/example/desktopclient/Application.java
@@ -2,6 +2,7 @@ package org.example.desktopclient;
 
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.example.desktopclient.scene.GameCatalogScene;
 
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
+
 
         primaryStage.setTitle("GameVault");
         primaryStage.setScene(new GameCatalogScene().createScene(primaryStage));
