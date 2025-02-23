39b7990488f0c32c2fe7c60552e70ad59dff7383
petaaar88 <pe.bn80@gmail.com>
2025-02-23 20:12:55 +0100

    Add searhing games in collection

--- src/main/java/org/example/desktopclient/controller/UsersGamesCollectionController.java
+++ src/main/java/org/example/desktopclient/controller/UsersGamesCollectionController.java
@@ -1,17 +1,21 @@
 package org.example.desktopclient.controller;
 
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import org.example.desktopclient.component.UserGameInCollectionComponent;
 import org.example.desktopclient.component.UsersGamesCollectionComponent;
+import org.example.desktopclient.model.game.GameInCollectionDTO;
 import org.example.desktopclient.service.game.GameService;
 
 import java.util.*;
 
-public class UsersGamesCollectionController {
+public class UsersGamesCollectionController implements ISearchable {
 
     private UsersGamesCollectionComponent component;
     private UserGameInCollectionDetailsController userGameInCollectionDetailsController;
+    private List<GameInCollectionDTO> gamesInUserCollection;
 
     private UserGameInCollectionController selectedGameInCollectionController;
     private List<UserGameInCollectionController> gamesInCollectionControllers;
@@ -20,8 +24,12 @@ public class UsersGamesCollectionController {
 
     public UsersGamesCollectionController(UsersGamesCollectionComponent component, UserGameInCollectionDetailsController userGameInCollectionDetailsController) {
         this.component = component;
+        SearchController searchController = new SearchController(component.getSearchComponent());
+        searchController.setSearchableController(this);
+
         this.userGameInCollectionDetailsController = userGameInCollectionDetailsController;
         gamesInCollectionControllers = new ArrayList<>();
+        gamesInUserCollection = new ArrayList<>();
 
         gameService = new GameService();
 
@@ -34,6 +42,8 @@ public class UsersGamesCollectionController {
             Platform.runLater(() -> {
 
                 gamesInCollection.forEach(game -> {
+
+                    gamesInUserCollection.add(game);
 
                     UserGameInCollectionComponent userGameInCollectionComponent = new UserGameInCollectionComponent();
 
@@ -86,5 +96,57 @@ public class UsersGamesCollectionController {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
+    }
+
+    @Override
+    public void search(String text) {
+        this.component.getGamesInCollectionVbox().getChildren().clear();
+        gamesInCollectionControllers.clear();
+
+        if(text.isBlank()){
+            gamesInUserCollection.forEach(game -> {
+
+                UserGameInCollectionComponent userGameInCollectionComponent = new UserGameInCollectionComponent();
+
+                userGameInCollectionComponent.setContent(game.getIcon(), game.getTitle());
+
+                Integer gameId = game.getId();
+
+                UserGameInCollectionController userGameInCollectionController = new UserGameInCollectionController(gameId, userGameInCollectionComponent, this);
+                component.getGamesInCollectionVbox().getChildren().add(userGameInCollectionComponent.getComponent());
+                gamesInCollectionControllers.add(userGameInCollectionController);
+
+                userGameInCollectionController.setSelected(false);
+            });
+        }
+        else{
+
+
+
+            gamesInUserCollection.forEach(game -> {
+
+                if(!game.getTitle().toLowerCase().contains(text.toLowerCase())) return;
+                UserGameInCollectionComponent userGameInCollectionComponent = new UserGameInCollectionComponent();
+
+                userGameInCollectionComponent.setContent(game.getIcon(), game.getTitle());
+
+                Integer gameId = game.getId();
+
+                UserGameInCollectionController userGameInCollectionController = new UserGameInCollectionController(gameId, userGameInCollectionComponent, this);
+                component.getGamesInCollectionVbox().getChildren().add(userGameInCollectionComponent.getComponent());
+                gamesInCollectionControllers.add(userGameInCollectionController);
+
+                userGameInCollectionController.setSelected(false);
+            });
+
+            if(gamesInUserCollection.stream().map(GameInCollectionDTO::getTitle).filter(title -> title.toLowerCase().contains(text.toLowerCase())).findAny().isEmpty()){
+                Label label = new Label("No results found");
+                label.setPadding(new Insets(0, 0, 0, 20));
+                label.setStyle("-fx-font-size: 15;-fx-font-weight: bold;-fx-text-fill: #0084FF;");
+                component.getGamesInCollectionVbox().getChildren().add(label);
+            }
+
+        }
+
     }
 }
